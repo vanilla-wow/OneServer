28b9366127176de706572e3746f10762255e266b
Salja <salja8840@gmail.com>
2014-01-20 14:18:32 +0100

    [DEV] Correct PvP Out of Combat Timer
    Signed-off-by: Salja <salja8840@gmail.com>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6922,7 +6922,7 @@ void Unit::SetInCombatState(bool PvP, Unit* enemy)
         return;
 
     if (PvP)
-        m_CombatTimer = 5000;
+        m_CombatTimer = 6000;
 
     if (isInCombat())
         return;
