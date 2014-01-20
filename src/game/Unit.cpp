bc90fd3d8c04fbf3e694d92ee6b652ae706c757d
Salja <salja8840@gmail.com>
2014-01-20 14:23:58 +0100

    [DEV] Fix Visual Stuck from Movement (example "fear")
    Signed-off-by: Salja <salja8840@gmail.com>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7542,11 +7542,14 @@ void Unit::SetSpeedRate(UnitMoveType mtype, float rate, bool forced)
             {MSG_MOVE_SET_FLIGHT_BACK_SPEED, SMSG_FORCE_FLIGHT_BACK_SPEED_CHANGE},
         };
 
-        if (forced && GetTypeId() == TYPEID_PLAYER)
+        if (forced)
         {
-            // register forced speed changes for WorldSession::HandleForceSpeedChangeAck
-            // and do it only for real sent packets and use run for run/mounted as client expected
-            ++((Player*)this)->m_forced_speed_changes[mtype];
+            if (GetTypeId() == TYPEID_PLAYER)
+            {
+                // register forced speed changes for WorldSession::HandleForceSpeedChangeAck
+                // and do it only for real sent packets and use run for run/mounted as client expected
+                ++((Player*)this)->m_forced_speed_changes[mtype];
+            }
 
             WorldPacket data(Opcodes(SetSpeed2Opc_table[mtype][1]), 18);
             data << GetPackGUID();
@@ -7554,18 +7557,18 @@ void Unit::SetSpeedRate(UnitMoveType mtype, float rate, bool forced)
             if (mtype == MOVE_RUN)
                 data << uint8(0);                           // new 2.1.0
             data << float(GetSpeed(mtype));
-
-            ((Player*)this)->GetSession()->SendPacket(&data);
+            SendMessageToSet(&data, true);
         }
+        else
+        {
+            m_movementInfo.UpdateTime(WorldTimer::getMSTime());
 
-        m_movementInfo.UpdateTime(WorldTimer::getMSTime());
-
-        // TODO: Actually such opcodes should (always?) be packed with SMSG_COMPRESSED_MOVES
-        WorldPacket data(Opcodes(SetSpeed2Opc_table[mtype][0]), 64);
-        data << GetPackGUID();
-        data << m_movementInfo;
-        data << float(GetSpeed(mtype));
-        SendMessageToSet(&data, false);
+            WorldPacket data(Opcodes(SetSpeed2Opc_table[mtype][0]), 64);
+            data << GetPackGUID();
+            data << m_movementInfo;
+            data << float(GetSpeed(mtype));
+            SendMessageToSet(&data, true);
+        }
     }
 
     CallForAllControlledUnits(SetSpeedRateHelper(mtype, forced), CONTROLLED_PET | CONTROLLED_GUARDIANS | CONTROLLED_CHARM | CONTROLLED_MINIPET);
