7d4cc2d0b518c2139965e493387f6a1178dd0dd6
Salja <salja8840@gmail.com>
2014-01-23 15:52:31 +0100

    [WARDEN] Implement Warden System. Thanks all Authors
    Signed-off-by: Salja <salja8840@gmail.com>

---
+++ sql/Warden/sxxxx_xxxxx_03_realmd_warden_data_result.sql
@@ -0,0 +1,5 @@
+INSERT INTO `warden_data_result` (`check`, `data`, `str`, `address`, `length`, `result`, `comment`) VALUES
+(243,'','',4840352,2,'558B','lua protection'),
+(243,'','',8979979,5,'E04D62503F','Hyper speed'),
+(243,'','',9208923,5,'C0854A3340','gravity'),
+(243,'','',8100062,2,'7541','air jump');
