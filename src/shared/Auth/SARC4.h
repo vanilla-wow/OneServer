d3c5970c7c604a764ee7cc3c3a22d5b5678ddd06
Salja <salja8840@gmail.com>
2014-01-23 16:06:44 +0100

    [WARDEN] Update Copyright
    Signed-off-by: Salja <salja8840@gmail.com>

--- src/shared/Auth/SARC4.h
+++ src/shared/Auth/SARC4.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2012 MaNGOS <http://getmangos.com/>
+ * This file is part of the CMaNGOS Project. See AUTHORS file for Copyright information
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
+ 
 #ifndef _AUTH_SARC4_H
 #define _AUTH_SARC4_H
 
