27c70d7f800cd8517b646560ab2036cf94fe9869
Salja <salja8840@gmail.com>
2014-03-17 15:33:55 +0100

    Eluna Fix Rochet´s Fix about Fix OnResurrect =) so now is fixed.
    Signed-off-by: Salja <salja8840@gmail.com>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -4257,6 +4257,8 @@ void Player::ResurrectPlayer(float restore_percent, bool applySickness)
     // update visibility of player for nearby cameras
     UpdateObjectVisibility();
 
+    sHookMgr->OnResurrect(this);
+
     if (!applySickness)
         return;
 
@@ -4283,7 +4285,6 @@ void Player::ResurrectPlayer(float restore_percent, bool applySickness)
             }
         }
     }
-    sHookMgr->OnResurrect(this);
 }
 
 void Player::KillPlayer()
