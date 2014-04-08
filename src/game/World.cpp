3f2b62ab1d61f097a56fca5fa161b5a744c075c0
Cyberium <cybermomo@techemail.com>
2014-04-08 17:31:46 +0200

    [s2092] Loading/reloading classlevelstats.
    Simple function to load and reload creature_template_classlevelstats table.
    Also add getter.
    Part 2/3 of new stats system.
    
    Thank to @Schmoozerd.

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -1021,6 +1021,9 @@ void World::SetInitialWorldSettings()
     sLog.outString("Loading Equipment templates...");
     sObjectMgr.LoadEquipmentTemplates();
 
+    sLog.outString("Loading Creature Stats...");
+    sObjectMgr.LoadCreatureClassLvlStats();
+
     sLog.outString("Loading Creature templates...");
     sObjectMgr.LoadCreatureTemplates();
 
