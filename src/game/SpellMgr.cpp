2e746da94f7fac7e2becc5e5742adff5e71896ac
Xfurry <xfurry@scriptdev2.com>
2014-11-15 00:56:33 +0100

    [s2197] Update some spells for Sunwell Plateau
    Dummy auras: 46637
    Stacking exception: 44852 and 46021
    
    (based on commit [12741] - 7572d74)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -2046,6 +2046,11 @@ bool SpellMgr::IsNoStackSpellDueToSpell(uint32 spellId_1, uint32 spellId_2) cons
                     if ((spellInfo_1->Id == 42280 && spellInfo_2->Id == 42294) ||
                             (spellInfo_2->Id == 42280 && spellInfo_1->Id == 42294))
                         return false;
+
+                    // Spectral Realm (reaction) and Spectral Realm (invisibility)
+                    if ((spellInfo_1->Id == 44852 && spellInfo_2->Id == 46021) ||
+                            (spellInfo_2->Id == 44852 && spellInfo_1->Id == 46021))
+                        return false;
                     break;
                 }
                 case SPELLFAMILY_MAGE:
