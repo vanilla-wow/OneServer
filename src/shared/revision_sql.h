c519e804135a1813541c6c4d83416058e7591bfc
ShinDarth <borzifrancesco@gmail.com>
2014-11-15 01:27:17 +0100

    [s2204] Implement Battleground scores storage system
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit [12756] - 9b7bb67)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
- #define REVISION_DB_CHARACTERS "required_s2063_12562_01_characters_various_tables"
+ #define REVISION_DB_CHARACTERS "required_s2204_12756_01_characters_pvpstats"
  #define REVISION_DB_MANGOS "required_s2201_12748_01_mangos_spell_template"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
  #define REVISION_DB_CHARACTERS_DEV "required_dev_0004_01_character_dev_db_version"
