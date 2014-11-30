7a1c7fe04c910be88e3f81b6296c0970206b7f44
Salja <salja8840@gmail.com>
2014-11-30 11:50:23 +0100

    [DEV] Typo from last Commit
    Signed-off-by: Salja <salja8840@gmail.com>

--- sql/Develop/dev_0007_01_mangos_instance_template.sql
+++ sql/Develop/dev_0007_01_mangos_instance_template.sql
@@ -1,3 +1,3 @@
 ALTER TABLE dev_db_version CHANGE COLUMN required_dev_0006_02_mangos_command required_dev_0007_01_mangos_instance_template BIT;
 
-ALTER TABLE `quest_template` DROP COLUMN `allowMount`;
+ALTER TABLE `instance_template` DROP COLUMN `allowMount`;
